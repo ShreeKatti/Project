943d32b20d630af232e4c6c1a56a0a2796964115
ShreeKatti <149763446+ShreeKatti@users.noreply.github.com>
2025-04-15 12:14:05 +0530

    Building pages

---
+++ megablog/src/appwrite/auth.js
@@ -0,0 +1,21 @@
+import conf from '../conf.js'
+import {Client,Account,ID} from "appwrite";
+
+export class AuthService{
+    client =new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try{
+            const userAccount
+        }
+    }
+}
+
